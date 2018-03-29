dca3bd5b0d7189e278bfbaeab8e2ede4a0cb84a2
Ryan Ye <yejianye@gmail.com>
2018-03-30 00:51:39 +0800

    New blog wip

--- publish_aliyun.sh
+++ publish_aliyun.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Build site..."
+jekyll build
+echo "Sync to aliyun..."
 rsync --verbose  --progress --stats --compress --rsh=ssh \
      --recursive --times --perms --delete \
      _site/* ryan-aliyun:/home/ryan/blog
